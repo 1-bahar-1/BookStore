bf9734150c3ad035f9906c92777e1aa7c692148e
ZahraBaharlouei <zbaharlo80@gmail.com>
2026-06-12 12:37:58 +0330

    add core models (Book, Author, Category, Keyword and relations)

---
+++ BookStore/Models/Book.cs
@@ -0,0 +1,21 @@
+﻿namespace BookStore.Models;
+
+public class Book
+{
+    public int Id { get; set; }
+
+    public string Title { get; set; }
+    public string Slug { get; set; }
+    public string FilePath { get; set; }
+    public int PageCount { get; set; }
+
+    
+    public int CategoryId { get; set; }
+    public Category Category { get; set; }
+
+    
+    public List<BookAuthor> BookAuthors { get; set; }
+    public List<BookKeyword> BookKeywords { get; set; }
+
+    public List<BookRelation> RelatedBooks { get; set; }
+}
---
+++ BookStore/Models/BookRelation.cs
@@ -0,0 +1,12 @@
+﻿namespace BookStore.Models;
+
+public class BookRelation
+{
+    public int Id { get; set; }
+
+    public int BookId { get; set; }
+    public Book Book { get; set; }
+
+    public int RelatedBookId { get; set; }
+    public Book RelatedBook { get; set; }
+}
